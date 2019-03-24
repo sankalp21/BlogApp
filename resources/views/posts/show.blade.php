79e5e3313ef03591335e8191601224ba55cecd73
Sankalp Sinha <sankalpsinha@Sankalps-MacBook-Air.local>
2019-03-24 22:55:19 +0530

    comment UI has been improved

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,7 @@
 @section('body')
     <a href="." class="btn btn-default">Go Back</a><br><br>
     <h1>{{$post->title}}</h1><br>
-    <img style="width:100%" src="../public/storage/cover_images/{{$post->cover_image}}"><br><br>
+    <img style="display: block; margin-left: auto; margin-right: auto;width:90%" src="../public/storage/cover_images/{{$post->cover_image}}"><br><br>
     <div class="container">
     <blockquote class="quote-card green-card">
               <p>
@@ -37,42 +37,26 @@
         @endif
         <br>
          <div class="alert alert-success" style="display:none"></div>
-        <div class="jumbotron">
-            <h4>Comment Box</h4>
-        
-             <div>
-              <medium>{{ Auth::user()->name }}</medium>
-               <form id="form">
-                   <div class="row">
-                       <input type="text" class="col-md-9 " name="comment" id="comment" required>
-                       <div class="col-md-1"></div>
-                       <button id="ajaxsave" class=" col-md-2 btn btn-primary">
-                           <ion-icon name="send"></ion-icon>
-                       </button>
-                   </div>
-               </form>
-            </div>
-            <hr>
-            
-                <div class="commentarea">
-                    @if(count($comments)>0)
-                       @foreach($comments as $comment)
-                           <medium>{{ $comment->user_name }}</medium>
-                           <p>{{ $comment->comment }}</p>
-                           <small>{{ $comment->created_at }}</small>
-                           <hr>
-                       
-                       @endforeach
-                    
-                    @else
-                       <p>"No comments found"</p>
-                    
-                    
-                    <div id="commentfetch"></div>
-                    @endif
+         
+        <div class="card">
+           <div class="card-header">Write your comment</div>
+            <div class="card-body">
+               <medium class="card-title">{{ Auth::user()->name }}</medium>
+                <div class="card-text">
+                   <form id="form">
+                       <div class="row">
+                           <img class="col-md-2" src="noimage.jpg">
+                           <div class="col-md-1"></div>
+                           <input type="text" class="col-md-7 " placeholder="Type your comment here..." name="comment" id="comment" required>
+                           <div class="col-md-1"></div>
+                           <button id="ajaxsave" class=" col-md-1 btn btn-primary">
+                               <ion-icon name="send"></ion-icon>
+                           </button>
+                       </div>
+                   </form>
                 </div>
-            
-            
+            </div>
+              
         </div>
         
         <script >
@@ -93,7 +77,13 @@
                          $('.alert').html(result.success);
                       }});
                     
-                    $.ajax({
+                    getComments();
+                    
+                   });
+            
+            
+            function getComments(){
+                $.ajax({
                          url:"{{ route('comments.show',$post->id) }}",
                          method:'GET',
                          dataType: 'json',
@@ -108,10 +98,19 @@
                                      var comment= response['data'][i].comment;
                                      var created_at=response['data'][i].created_at;
                                      
-                                     var comment_data="<medium>"+user+"</medium>"+
-                                                      "<p>"+comment+"</p>"+
-                                                      "<small>"+created_at+"</small>"+
-                                                      "<hr>";
+                                     var comment_data="<div class='card'>"+
+                                                         "<div class='row'>"+
+                                                            "<img class='col-md-1'>"+
+                                                            "<div class='col-md-1'></div>"+
+                                                            "<div class='card-body col-md-10 '>"+
+                                                                "<medium class='card-title'>"+
+                                                                    "<span>"+user+"</span>&nbsp;&nbsp;&nbsp;"+
+                                                                    "<small>"+created_at+"</small>"+
+                                                                "</medium>"+
+                                                                "<p class='card-text'>"+comment+"</p>"+
+                                                            "</div>"+
+                                                         "</div>"+
+                                                       "</div>";
                                      $('.commentarea').append(comment_data,"");
                                  }
                                  
@@ -120,15 +119,44 @@
                                }
                          }
                      });
-                    
-                   });
+            }
         
         
         </script>
         
         
    @endif
-    
+   <br>
+   <div class="card">
+       <h5 class="card-header"><strong>Other Comments</strong></h5>
+        <div class="commentarea">
+                        @if(count($comments)>0)
+                           @foreach($comments as $comment)
+                              <div class="card">
+                                 <div class="row">
+                                     <img class="col-md-1">
+                                     <div class="col-md-1"></div>
+                                     <div class="card-body col-md-10 ">
+                                     <medium class="card-title">
+                                         <span>{{ $comment->user_name }}</span>&nbsp;&nbsp;&nbsp;
+                                         <small>{{ $comment->created_at }}</small>
+                                     </medium>
+                                     <p class="card-text">{{ $comment->comment }}</p>
+                                     </div>
+                                 </div>
+                            
+                              </div>
+                              
+                           @endforeach
+
+                        @else
+                           <p>"No comments found"</p>
+
+
+                        <div id="commentfetch"></div>
+                        @endif
+         </div>
+  </div>
        
  
 @endsection
